114155856d1ff76ad766efbb66fb270f20006c32
romek <contact@romek.codes>
2025-08-25 22:14:41 +0200

    hotfix: require telescope actions and actions.state

--- lua/bruno/init.lua
+++ lua/bruno/init.lua
@@ -97,6 +97,8 @@ local env_pickers = {
 		local pickers = require("telescope.pickers")
 		local finders = require("telescope.finders")
 		local conf = require("telescope.config").values
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
 		pickers
 			.new({}, {
 				prompt_title = prompt,
